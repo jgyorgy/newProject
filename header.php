29ae2ddba2a537eb8880f0db0043c2d5232c2f95
jgyorgy <jozsef_gyorgy@yahoo.com>
2014-07-21 15:12:51 +0300

    create

---
+++ header.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Project</title>
+		<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+		<link href="css/jquery.selectbox.css" type="text/css" rel="stylesheet" />
+		<link href="css/style.css" type="text/css" rel="stylesheet" />
+		<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
+		<script type="text/javascript" src="js/jquery.selectbox-0.2.js"></script>
+		<script type="text/javascript">
+		$(function () {
+			$("#country_id").selectbox();
+		});
+		</script>
+	</head>
+	<body>
+	
+		<ul class="headerMenu">
+			<li>Home</li>
+			<li>Restaurante</li>
+			<li>Despre</li>
+			<li>Ajutor</li>
+		</ul>
