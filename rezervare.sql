61d7abe2301d03e35ad29818da44079ec9dce82f
snalkum <mail@adrianticusan.eu>
2015-02-27 20:36:52 +0200

    DB

--- rezervare.sql
+++ rezervare.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.2.2
+-- version 4.2.6deb1
 -- http://www.phpmyadmin.net
 --
--- Gazda: 127.0.0.1
--- Timp de generare: 12 Aug 2014 la 19:19
--- Versiune server: 5.5.27
--- Versiune PHP: 5.4.7
+-- Host: localhost
+-- Generation Time: Feb 27, 2015 at 08:36 PM
+-- Server version: 5.5.41-0ubuntu0.14.10.1
+-- PHP Version: 5.5.12-2ubuntu4.2
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -17,23 +17,22 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Baza de date: `rezervare`
+-- Database: `rezervare`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structura de tabel pentru tabelul `mese`
+-- Table structure for table `mese`
 --
 
 CREATE TABLE IF NOT EXISTS `mese` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `numar_mese` int(11) NOT NULL,
-  PRIMARY KEY (`ID`)
+`ID` int(11) NOT NULL,
+  `numar_mese` int(11) NOT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
--- Salvarea datelor din tabel `mese`
+-- Dumping data for table `mese`
 --
 
 INSERT INTO `mese` (`ID`, `numar_mese`) VALUES
@@ -44,35 +43,40 @@ INSERT INTO `mese` (`ID`, `numar_mese`) VALUES
 -- --------------------------------------------------------
 
 --
--- Structura de tabel pentru tabelul `mese_disponibile`
+-- Table structure for table `mese_disponibile`
 --
 
 CREATE TABLE IF NOT EXISTS `mese_disponibile` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
+`ID` int(11) NOT NULL,
   `Restaurant_ID` int(11) NOT NULL,
   `Mese_ID` int(11) NOT NULL,
-  `mese_disponibile` int(11) NOT NULL,
-  `data` date NOT NULL,
-  `ora` int(11) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `data` datetime NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `mese_disponibile`
+--
+
+INSERT INTO `mese_disponibile` (`ID`, `Restaurant_ID`, `Mese_ID`, `data`) VALUES
+(3, 1, 2, '2015-02-03 01:00:00'),
+(4, 1, 2, '2015-02-03 02:00:00'),
+(6, 1, 2, '2015-02-03 02:35:00');
 
 -- --------------------------------------------------------
 
 --
--- Structura de tabel pentru tabelul `nr_mese`
+-- Table structure for table `nr_mese`
 --
 
 CREATE TABLE IF NOT EXISTS `nr_mese` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
+`ID` int(11) NOT NULL,
   `Restaurant_ID` int(11) NOT NULL,
   `Mese_ID` int(11) NOT NULL,
-  `mese_totale` int(11) NOT NULL,
-  PRIMARY KEY (`ID`)
+  `mese_totale` int(11) NOT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
--- Salvarea datelor din tabel `nr_mese`
+-- Dumping data for table `nr_mese`
 --
 
 INSERT INTO `nr_mese` (`ID`, `Restaurant_ID`, `Mese_ID`, `mese_totale`) VALUES
@@ -84,25 +88,76 @@ INSERT INTO `nr_mese` (`ID`, `Restaurant_ID`, `Mese_ID`, `mese_totale`) VALUES
 -- --------------------------------------------------------
 
 --
--- Structura de tabel pentru tabelul `restaurante`
+-- Table structure for table `restaurante`
 --
 
 CREATE TABLE IF NOT EXISTS `restaurante` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
+`ID` int(11) NOT NULL,
   `Nume` text NOT NULL,
   `Adresa` text NOT NULL,
-  `Thumbnail` text NOT NULL,
-  PRIMARY KEY (`ID`)
+  `Thumbnail` text NOT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
--- Salvarea datelor din tabel `restaurante`
+-- Dumping data for table `restaurante`
 --
 
 INSERT INTO `restaurante` (`ID`, `Nume`, `Adresa`, `Thumbnail`) VALUES
 (1, 'aaa', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', ''),
 (2, 'bbbbbbbbbbbb', 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb', '');
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mese`
+--
+ALTER TABLE `mese`
+ ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `mese_disponibile`
+--
+ALTER TABLE `mese_disponibile`
+ ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `nr_mese`
+--
+ALTER TABLE `nr_mese`
+ ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `restaurante`
+--
+ALTER TABLE `restaurante`
+ ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `mese`
+--
+ALTER TABLE `mese`
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `mese_disponibile`
+--
+ALTER TABLE `mese_disponibile`
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `nr_mese`
+--
+ALTER TABLE `nr_mese`
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `restaurante`
+--
+ALTER TABLE `restaurante`
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
